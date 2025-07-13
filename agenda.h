bccc947e3dfba314df4b448c2e9821b15fdabf72
laribarreto <larissa.barreto.106@ufrn.edu.br>
2025-07-13 18:45:50 -0300

    Create agenda.h

---
+++ agenda.h
@@ -0,0 +1,11 @@
+#ifndef AGENDA_H
+#define AGENDA_H
+
+#include "corretor.h"
+#include "imovel.h"
+#include <vector>
+#include <map>
+
+void agendarVisitas(std::vector<Corretor>& corretores, const std::vector<Imovel>& imoveis);
+
+#endif
